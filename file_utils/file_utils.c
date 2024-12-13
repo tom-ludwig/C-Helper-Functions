18687affcb5e4eaf797553b523ff22bb8a747a06
dev-laky <laky.dev@proton.me>
2024-12-13 12:00:41 +0100

    chore: copyright comment added to whole project scope

--- file_utils/file_utils.c
+++ file_utils/file_utils.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2024
+ * Created by Tom M. Ludwig, Louis Kauer, Julian Kuecken, and Pepe Hanisch
+ *
+ * This project is licensed under the MIT License.
+ * You may use, modify, and distribute this software under the terms of the license.
+*/
+
 #include "file_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
